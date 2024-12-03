df02a49940f37f62788aeb8ab0d611a605ca37f3
ialeksee <ivaylo.alekseev@gmail.com>
2024-12-03 10:47:14 +0100

    updated with generic read file function

--- aoc.cpp
+++ aoc.cpp
@@ -1,65 +1,29 @@
 #include "util.h"
-#include <vector>
 #include <iostream>
-#include <algorithm>
 #include <cstdlib>
 
 int main()
 {
-  std::ifstream ifs;
-  std::string str;
-  std::vector<unsigned int> left;
-  std::vector<unsigned int> right;
-  unsigned int number;
-  bool isRight = false;
-  ifs.open("day1.txt");
-
-  if(ifs)
-  {
-    while(!ifs.eof())
+    std::vector<unsigned int> test_vector;
+    std::vector<unsigned int> left;
+    std::vector<unsigned int> right;
+    Util::readFile<unsigned int>("day1.txt", test_vector);
+    bool is_left = true;
+    for(auto num : test_vector)
     {
-      std::getline(ifs, str);
-
-      std::stringstream ss;
-      ss << str;
-
-      while(ss >> number)
-      {
-	if(isRight)
-	{
-	  right.push_back(number);
-	}
-	else
-	{
-	  left.push_back(number);
-	}
-	isRight = !isRight;
-      }
-    }
-    
+      (is_left) ? left.push_back(num) : right.push_back(num);
+      is_left = !is_left;
+    }  
     std::stable_sort(left.begin(), left.end());
     std::stable_sort(right.begin(), right.end());
     unsigned int distance_total{0};
     auto lit = left.begin();
-    auto rit = right.begin();
-    for(;lit != left.end(); ++lit, ++rit)
+    
+    for(;lit != left.end(); ++lit)
     {
-      distance_total += std::abs(int(*lit - *rit));
+      unsigned int num = *lit;
+      
+      distance_total += num*std::count(right.begin(), right.end(), num);
     }
-
-    std::cout << "Distance: "<< distance_total << std::endl;
-    /*
-    std::cout << "Left array" << std::endl;
-    for(auto num : left)
-    {
-      std::cout << num << std::endl;
-    }
-
-    std::cout << "Right array" << std::endl;
-    for(auto num : right)
-    {
-      std::cout << num << std::endl;
-    }
-    */
-  }
+    std::cout << distance_total << std::endl; 
 }
--- util.h
+++ util.h
@@ -5,17 +5,39 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-
+#include <vector>
+#include <algorithm>
 
 class Util
 {
-  Util(){};
-  static void readFile(std::string filename);
+  public:
+    Util(){};
+    template <typename T> static void readFile(std::string filename, std::vector<T> &out);
 };
 
-void Util::readFile(std::string filename)
+template <typename T> void Util::readFile(std::string filename, std::vector<T> &out)
 {
-  
+  std::ifstream ifs;
+  std::string str;
+  T token;
+
+  ifs.open(filename);
+
+  if(ifs)
+  {
+    while(!ifs.eof())
+    {
+      std::getline(ifs, str);
+
+      std::stringstream ss;
+      ss << str;
+
+      while(ss >> token)
+      {
+	out.push_back(token);
+      }
+    }
+  } 
 }
 
 #endif
