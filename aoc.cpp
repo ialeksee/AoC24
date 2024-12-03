697914e29a90febaa3b1ee8991a7980ef5936802
ialeksee <ivaylo.alekseev@gmail.com>
2024-12-03 16:17:47 +0100

    day2 finally completed

--- aoc.cpp
+++ aoc.cpp
@@ -31,58 +31,122 @@ void day1()
 void day2()
 {
   int safe_reports = 0;
+  int old_safe_reps = 0;
+  int new_safe_reps = 0;
   std::vector<std::vector<unsigned int>> reports;
   Util::readByLine<unsigned int>("day2.txt", reports);
 
   for(auto report : reports)
   {
     bool is_safe = true;
-    bool is_really_safe = true;
-    bool is_ascending = false;
-    bool is_descending = false;
-    if(report.empty())
+    bool is_dampened = false;
+   if(report.empty())
       break;
-    auto iter = report.begin();
-    while(iter + 1 != report.end())
+//    std::cout << "Initial report: ";
+//    for(auto num : report)
+//      std::cout << num << " ";
+//    std::cout << std::endl;
+    auto check_safety = [&](std::vector<unsigned int> rep)
     {
-      auto diff = std::abs((int)(*(iter + 1) - *iter));
-      if((diff == 0)||(diff > 3))
+      auto iter = rep.begin();
+      bool is_ascending = false;
+      bool is_descending = false;
+    
+      while(iter + 1 != rep.end())
       {
-	is_safe = false;
-	break;
-      }
-      if(*(iter + 1) > *iter)
-      {
-	if(!is_descending && !is_ascending)
+	//std::cout << *iter << " ";
+	auto diff = std::abs((int)(*(iter + 1) - *iter));
+	if((diff == 0)||(diff > 3))
 	{
-	  is_ascending = true;
-	}
-	else if(is_descending)
-	{  
 	  is_safe = false;
+//	  std::cout << "Diff is: " << diff << std::endl;
+	}
+	if(*(iter + 1) > *iter)
+	{
+	  if(!is_descending && !is_ascending)
+	  {
+	    is_ascending = true;
+	  }
+	  else if(is_descending)
+	  {  
+	    is_safe = false;
+//	    std::cout << "Ascending broken"  << std::endl;
+	  }
+	}
+	
+	if(*(iter + 1) < *iter)
+	{
+	  if(!is_descending && !is_ascending)
+	  {
+	    is_descending = true;
+	  }
+	  else if(is_ascending)
+	  {  
+	    is_safe = false;
+//		  std::cout << "Descending broken" << std::endl;
+	  }
+	}
+	if(!is_safe)
+	{
 	  break;
 	}
+	iter++;
       }
-      
-      if(*(iter + 1) < *iter)
-      {
-	if(!is_descending && !is_ascending)
-	{
-	  is_descending = true;
-	}
-	else if(is_ascending)
-	{  
-	  is_safe = false;
-	  break;
-	}
-      }
-      iter++;
-    }
+    };
+    check_safety(report);
+ //   std::cout << std::endl;
     if(is_safe)
+    {
+//      std::cout << "Initial report: ";
+//      for(auto num : report)
+//	std::cout << num << " ";
+// ..     std::cout << std::endl;
+      old_safe_reps++;
       safe_reports++;
+    }
+    else
+    {
+      std::vector<unsigned int> copy_vec;
+      auto it = report.begin();
+      int idx = 0;
+      while(it != report.end())
+      {
+	copy_vec = report;
+	is_safe = true;
+	copy_vec.erase(copy_vec.begin() + idx);
+	//std::cout << "Copy vector: ";
+	//for(auto num : copy_vec)
+	 // std::cout << num << " ";
+	//std::cout << std::endl;
+	check_safety(copy_vec);
+	//std::cout << std::endl;
+//	if(is_safe)
+/*	{
+	  std::cout << "Original report: ";
+	  for(auto num : report)
+	    std::cout << num << " ";
+	  std::cout << std::endl;
+	  
+	  std::cout << "Copy vector: ";
+	  for(auto num : copy_vec)
+	    std::cout << num << " ";
+	  std::cout << std::endl;
+*/
+	if(is_safe)
+	{
+	  new_safe_reps++;
+	  safe_reports++;
+	  break;
+	}
+	it++;
+	idx++;
+      }
+    }
   }
 
-  std::cout << safe_reports << std::endl;
+   std::cout << old_safe_reps << std::endl;
+   std::cout << new_safe_reps << std::endl;
+   std::cout << safe_reports << std::endl;
 }
 
 int main()
